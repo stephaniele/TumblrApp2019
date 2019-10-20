3b7927678a187c89d55d55d927ad251a352571eb
Stephanie Le <tle3@macalester.edu>
2019-10-19 19:16:03 -0500

    Initial Commit

---
+++ TumblrApp2019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TumblrApp2019
+//
+//  Created by Stephanie Le on 10/19/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TumblrApp2019Tests/TumblrApp2019Tests.swift
@@ -0,0 +1,34 @@
+//
+//  TumblrApp2019Tests.swift
+//  TumblrApp2019Tests
+//
+//  Created by Stephanie Le on 10/19/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+import XCTest
+@testable import TumblrApp2019
+
+class TumblrApp2019Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
